c73ce823cfbe7a0cc4c62f6e1c403ea3ab0102a7
mahikero <federicoparassarmiento@yahoo.com>
2015-11-03 15:35:59 -0500

    up

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public String Index()
+        {
+            return "This is my <b>default</b> actioon..";
+        }
+
+        public String Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode( "Hello " + name + ", ID: " + ID);
+        }
+    }
+}
